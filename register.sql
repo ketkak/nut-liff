ff4e277277b5a047f6ee48e388a537dd4bbaabc9
ketkak <96605017+ketkak@users.noreply.github.com>
2022-01-18 10:34:23 +0700

    Delete register.sql

--- register.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS register;
-CREATE TABLE IF NOT EXISTS users (
-id int(3) NOT NULL AUTO_INCREMENT,
-user_id varchar(20) DEFAULT NULL,
-name varchar(300) DEFAULT NULL,
-samnak varchar(300) DEFAULT NULL,
-date datetime DEFAULT NULL,
-PRIMARY KEY (id)
-);
